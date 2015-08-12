e83c75f4527a2e197a2002c5d1c235742509bf67
Andrew Chung <andrew.chung159@gmail.com>
2015-08-11 23:16:56 -0400

    create dynamic grid and grid boxes

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	createGrid(5, 5);
+	boxHover();
+});
+
+var createGrid = function(rows, cols) {
+	var width = (900 - cols * 2) / cols + 'px';
+	var height = (600 - rows * 2) / rows + 'px';
+
+	for (var i = 0; i < rows * cols; i++) {
+		$('#grid-container').append('<div></div>');
+	};
+
+	$('div').css({height: height, width: width}).addClass('grid-box');
+};
+
+var boxHover = function() {
+	$('.grid-box').hover(function() {
+		$(this).slideToggle();
+	});
+};
